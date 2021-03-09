0247bde357d0229c16bad8d7fb15581b8e7187aa
palhamel <pal.hamel@gmail.com>
2021-03-09 20:55:29 +0100

    Change to ES6 imports and add script

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-const app = require('express')()
+// const app = require('express')()
+
+import express from 'express'
+
+const app = express()
+
+// app.get('/', (req, res) =>
+//   res.json({message: 'Docker is working great'})
+// )
 
 app.get('/', (req, res) =>
-  res.json({message: 'Docker is working great'})
+  res.send('Hello world - backend here! 🔥 Docker is great!')
 )
 
+// Defines the port the app will run on:
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`✅ app is running on http://localhost:${port}`))
